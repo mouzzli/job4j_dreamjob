2569fc7d4c6de5dce1060e80f9672bede08ce02a
Mouzzli <cokes03@yandex.ru>
2022-10-27 12:31:59 +0300

    Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -0,0 +1,69 @@
+package ru.job4j.dreamjob.model;
+
+import ru.job4j.dreamjob.util.Util;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+
+    private LocalDateTime created;
+
+    public Post() {
+    }
+
+    public Post(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = LocalDateTime.now();
+    }
+
+    public String getCreated() {
+        return created.format(Util.getFormatter());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
