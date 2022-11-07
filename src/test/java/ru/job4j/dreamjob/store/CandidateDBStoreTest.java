437d9b13c75ec7e0c612a8450b5b44d5376e468c
Mouzzli <cokes03@yandex.ru>
2022-11-07 16:47:17 +0300

    Тестирование базы данных. Liquibase H2 [#504862]

---
+++ src/test/java/ru/job4j/dreamjob/store/CandidateDBStoreTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.dreamjob.store;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.Main;
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.City;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class CandidateDBStoreTest {
+    @Test
+    public void whenCreateCandidate() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate = new Candidate(0, "Candidate name", "Description", new City(1, "Москва"));
+        store.addCandidate(candidate);
+        Candidate candidateInDB = store.findById(candidate.getId());
+        assertThat(candidateInDB.getName(), is(candidate.getName()));
+        assertThat(candidateInDB.getDescription(), is(candidate.getDescription()));
+        assertThat(candidateInDB.getCity().getId(), is(candidate.getCity().getId()));
+    }
+
+    @Test
+    public void whenUpdateCandidate() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate = new Candidate(0, "Candidate name", "Description", new City(1, "Москва"));
+        store.addCandidate(candidate);
+        candidate.setName("New Name");
+        store.update(candidate);
+        Candidate candidateInDB = store.findById(candidate.getId());
+        assertThat(candidateInDB.getName(), is(candidate.getName()));
+    }
+
+    @Test
+    public void whenFindAllCandidate() {
+        CandidateDBStore store = new CandidateDBStore(new Main().loadPool());
+        Candidate candidate1 = new Candidate(0, "Candidate name1", "Description1", new City(1, "Москва"));
+        Candidate candidate2 = new Candidate(0, "Candidate name2", "Description2", new City(1, "Москва"));
+        Candidate candidate3 = new Candidate(0, "Candidate name3", "Description3", new City(1, "Москва"));
+        List<Candidate> listCandidatesBefore = store.findAll();
+        store.addCandidate(candidate1);
+        store.addCandidate(candidate2);
+        store.addCandidate(candidate3);
+        List<Candidate> listCandidatesAfter = store.findAll();
+        assertThat(listCandidatesAfter.size() - listCandidatesBefore.size(), is(3));
+    }
+}
---
+++ src/test/java/ru/job4j/dreamjob/store/PostDBStoreTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.dreamjob.store;
+
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.dreamjob.Main;
+import ru.job4j.dreamjob.model.City;
+import ru.job4j.dreamjob.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PostDBStoreTest {
+    @Test
+    public void whenCreatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(0, "Java Job", "Description", new City(1, "Москва"), LocalDateTime.now(), true);
+        store.addPost(post);
+        Post postInDb = store.findById(post.getId()).get();
+        assertThat(postInDb.getName(), is(post.getName()));
+        assertThat(postInDb.getDescription(), is(post.getDescription()));
+        assertThat(postInDb.getCity().getId(), is(post.getCity().getId()));
+        assertThat(postInDb.isVisible(), is(post.isVisible()));
+    }
+
+    @Test
+    public void whenUpdatePost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post = new Post(0, "Java Job", "Description", new City(1, "Москва"), LocalDateTime.now(), true);
+        store.addPost(post);
+        post.setName("New Name");
+        store.update(post);
+        Post postInDb = store.findById(post.getId()).get();
+        assertThat(postInDb.getName(), is(post.getName()));
+    }
+
+    @Test
+    public void whenFindAllPost() {
+        PostDBStore store = new PostDBStore(new Main().loadPool());
+        Post post1 = new Post(0, "Java Job1", "Description1", new City(1, "Москва"), LocalDateTime.now(), true);
+        Post post2 = new Post(1, "Java Job2", "Description2", new City(1, "Москва"), LocalDateTime.now(), true);
+        Post post3 = new Post(2, "Java Job3", "Description3", new City(1, "Москва"), LocalDateTime.now(), true);
+        List<Post> postsBefore = store.findAll();
+        store.addPost(post1);
+        store.addPost(post2);
+        store.addPost(post3);
+        List<Post> posts = store.findAll();
+        assertThat(posts.size() - postsBefore.size(), is(3));
+    }
+}
