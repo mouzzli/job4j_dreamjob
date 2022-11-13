3fd304b68b4069db417f6d17a4733a71c5638511
Mouzzli <cokes03@yandex.ru>
2022-11-13 13:09:06 +0300

    Filter [#504865 #359930]

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class AuthFilter implements Filter {
+    private static final String LOGIN_PAGE = "loginPage";
+    private static final String LOGIN = "login";
+    private static final String FORM_ADD_USER = "formAddUser";
+    private static final String REGISTRATION = "registration";
 
     @Override
     public void doFilter(
@@ -22,11 +27,11 @@ public class AuthFilter implements Filter {
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
         Set<String> filterValues = new HashSet<>(Arrays.asList(
-                "loginPage",
-                "login",
-                "formAddUser",
-                "registration"));
-        if (filterValues.stream().anyMatch(uri::endsWith)) {
+                LOGIN_PAGE,
+                LOGIN,
+                FORM_ADD_USER,
+                REGISTRATION));
+        if (filter(uri, filterValues)) {
             chain.doFilter(req, res);
             return;
         }
@@ -35,5 +40,9 @@ public class AuthFilter implements Filter {
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    private boolean filter(String uri, Collection<String> values) {
+        return values.stream().anyMatch(uri::endsWith);
     }
 }
